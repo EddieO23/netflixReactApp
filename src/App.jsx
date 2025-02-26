3ae15ed535f243c95d99dd127690e1574a198610
Eddie <eocampo52@gmail.com>
2025-02-26 14:33:12 -0600

    succesfully fetching random movie from api to home page. added random movie function to Home page. AI helped me to dedbu and fetch api at home page.

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
-import React from 'react';
-import Home from './pages/Home';
-import Watch from './pages/Watch';
-import MyList from './pages/MyList';
-import Search from './pages/Search';
-import NotFound from './pages/NotFound'
+import React from "react";
+import Home from "./pages/Home";
+import Watch from "./pages/Watch";
+import MyList from "./pages/MyList";
+import Search from "./pages/Search";
+import NotFound from "./pages/NotFound";
 
-import { Link, BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './components/Navbar';
+import { Link, BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import { MovieProvider } from "./context/MovieContext";
 
 function App() {
   return (
-    <Router>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/watch' element={<Watch />} />
-        <Route path='/myList' element={<MyList />} />
-        <Route path='/search' element={<Search />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
-    </Router>
+    <MovieProvider>
+      <Router>
+        <MainContent />
+      </Router>
+    </MovieProvider>
   );
 }
 
 export default App;
+
+const MainContent = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/watch" element={<Watch />} />
+        <Route path="/myList" element={<MyList />} />
+        <Route path="/search" element={<Search />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,18 @@
-// import React, { useEffect, useState } from "react";
+// import React, { useContext, useEffect, useState } from "react"; // Added useState import
 // import { tmdbApi } from "../tmdbApi";
+// import { useMovieContext } from "../context/MovieContext";
 
 // function Home() {
-//   const [genresWithMovies, setgenresWithMovies] = useState(null);
+//   const {selectedMovie,
+//     setSelectedMovie,
+//     popularMovies,
+//     setPopularMovies,
+//     topRatedMovies,
+//     setTopRatedMovies,
+//     trendingMovies,
+//     setTrendingMovies} = useMovieContext()
+  
+//   const [genresWithMovies, setGenresWithMovies] = useState(null); // Corrected variable name to camelCase
 
 //   useEffect(() => {
 //     const loadMovies = async () => {
@@ -20,19 +30,45 @@
 
 //       if (allGenres.error) {
 //         console.log(allGenres.error);
-//         setgenresWithMovies([]);
-//       } else if(allGenres.data) {
+//         setGenresWithMovies([]); // Corrected variable name to camelCase
+//       } else if (allGenres.data) {
 //         const allGenreWithMovies = await Promise.all(
 //           allGenres.data.genres.map(async (genre) => {
-//             const movies = await tmdbApi.getMoviesByGenre(genre.id
-//               return {
-//                 id: genre.id,
-//                 name:genre.name,
-//                 movies: movies.data.results
-//               })
+//             const movies = await tmdbApi.getMoviesByGenre(genre.id); // Added missing closing parenthesis
+//             return {
+//               id: genre.id,
+//               name: genre.name,
+//               movies: movies.data.results,
+//             };
 //           })
-//         )
+//         );
+//         setGenresWithMovies(allGenreWithMovies)
 //       }
+
+//       if(popularMoviesResult.error){
+//         // set the popular movies to an empty array
+//         setPopularMovies([])
+//       } else {
+//         setPopularMovies(popularMovies.data.results)
+//       } 
+//       if (topRatedMovies.error) {
+//         setTopRatedMovies([])
+//       } else {
+//         setTopRatedMovies(topRatedMovies.data.results)
+//       }
+//       if (trendingMovies.error) {
+//         setTrendingMovies([])
+//       } else {
+//         setTrendingMovies(setTrendingMovies.data.results)
+//       }
+
+//       const randomIndex = Math.floor(Math.random() * popularMoviesResult.data?.results.length ) 
+
+//       const randomMovie = popularMoviesResult.data?.results(randomIndex)
+
+//       setSelectedMovie(randomMovie)
+
+//       console.log("random movie",randomMovie)
 //     };
 
 //     loadMovies();
@@ -44,11 +80,21 @@
 // export default Home;
 
 
-import React, { useEffect, useState } from "react"; // Added useState import
+import React, { useContext, useEffect, useState } from "react"; 
 import { tmdbApi } from "../tmdbApi";
+import { useMovieContext } from "../context/MovieContext";
 
 function Home() {
-  const [genresWithMovies, setGenresWithMovies] = useState(null); // Corrected variable name to camelCase
+  const { selectedMovie,
+    setSelectedMovie,
+    popularMovies,
+    setPopularMovies,
+    topRatedMovies,
+    setTopRatedMovies,
+    trendingMovies,
+    setTrendingMovies } = useMovieContext();
+  
+  const [genresWithMovies, setGenresWithMovies] = useState(null);
 
   useEffect(() => {
     const loadMovies = async () => {
@@ -66,11 +112,11 @@ function Home() {
 
       if (allGenres.error) {
         console.log(allGenres.error);
-        setGenresWithMovies([]); // Corrected variable name to camelCase
+        setGenresWithMovies([]);
       } else if (allGenres.data) {
         const allGenreWithMovies = await Promise.all(
           allGenres.data.genres.map(async (genre) => {
-            const movies = await tmdbApi.getMoviesByGenre(genre.id); // Added missing closing parenthesis
+            const movies = await tmdbApi.getMoviesByGenre(genre.id);
             return {
               id: genre.id,
               name: genre.name,
@@ -78,17 +124,36 @@ function Home() {
             };
           })
         );
-        setGenresWithMovies(allGenreWithMovies)
+        setGenresWithMovies(allGenreWithMovies);
       }
 
-      if(popularMoviesResult.error){
-        // set the popular movies to an empty array
-        
+      if (popularMoviesResult.error) {
+        setPopularMovies([]);
+      } else {
+        setPopularMovies(popularMoviesResult.data.results);
+      } 
+      if (topRatedMoviesResult.error) {
+        setTopRatedMovies([]);
+      } else {
+        setTopRatedMovies(topRatedMoviesResult.data.results);
       }
+      if (trendingMoviesResult.error) {
+        setTrendingMovies([]);
+      } else {
+        setTrendingMovies(trendingMoviesResult.data.results);
+      }
+
+      const randomIndex = Math.floor(Math.random() * popularMoviesResult.data?.results.length);
+
+      const randomMovie = popularMoviesResult.data?.results[randomIndex];
+
+      setSelectedMovie(randomMovie);
+
+      console.log("random movie", randomMovie);
     };
 
     loadMovies();
-  }, []);
+  }, [setSelectedMovie, setPopularMovies, setTopRatedMovies, setTrendingMovies]);
 
   return <div>HOME</div>;
 }
