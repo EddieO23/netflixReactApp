ed31269e26248ae38ea77861584178e6104a411f
Eddie <eocampo52@gmail.com>
2025-03-03 22:10:57 -0600

    finished myList feature of app. Next going to be working on search functionality. Left off on lecture 53

--- src/pages/MyList.jsx
+++ src/pages/MyList.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useUtilsContext} from '../context/UtilsContext'
+import Card from '../components/Card'
 
 function MyList() {
+  const {movieList} = useUtilsContext() 
+
+  useEffect(()=>{
+
+  }, [movieList])
   return (
-    <div>
-      MY LIST
+    <div className='absolute top-36 flex flex-wrap px-12 justify-around gap-4'>
+      {movieList.length > 0 ? (
+        movieList.map((movie, index) => (
+          <Card item={movie} key={index} />
+        ))
+      ) : (
+        <p className='text-white text-xl'>No movies found in your list...</p>
+      )}
     </div>
   )
 }
